0b3445eb951f324942789b25b09690e0551e3119
unknown <AtlHo@KBG6936.kongsberg.master.int>
2015-11-01 15:31:44 +0100

    Workng on workflow list

--- Blodbanken/Controls/WorkFlowControl.ascx.cs
+++ Blodbanken/Controls/WorkFlowControl.ascx.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Blodbanken.App_Code;
 
 namespace Blodbanken.Controls {
    public partial class WorkFlowControl : System.Web.UI.UserControl {
+      AuthenticatonModule AuthMod = new AuthenticatonModule();
       protected void Page_Load(object sender, EventArgs e) {
 
       }
